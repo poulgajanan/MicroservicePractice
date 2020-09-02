d6ee543ef4db2a3115db7e4f5d72611dd19ca29e
poulgajanan <55170140+gajananpoul@users.noreply.github.com>
2020-09-02 12:47:35 +0530

    some files added

---
+++ src/test/java/com/gp/FreshGoods/OnlineShopping/OnlineShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gp.FreshGoods.OnlineShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
